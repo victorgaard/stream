1e9465ff405c117e1210174846923a3b9fb9dc8d
Victor F. Santos <victor.gaard@gmail.com>
2025-06-10 15:23:36 -0300

    feat: custom discord icons

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,18 +1,47 @@
+import { HashtagIcon } from "./icons/HashtagIcon";
+
+const Members = () => {
+  return (
+    <aside className="border-slate-6 bg-slate-2 w-64 border-l pt-5 pl-4">
+      members
+    </aside>
+  );
+};
+
+const Messages = () => {
+  return <div className="bg-slate-2 flex-1 pt-5 pl-4">messages</div>;
+};
+
+const Channels = () => {
+  return <div className="w-[304px] pt-5 pl-4">hey</div>;
+};
+
 const Navbar = () => {
   return (
     <div className="flex items-center">
       <div className="border-slate-5 flex h-12 w-[304px] items-center border-b px-4">
-        <p className="text-base">Stream</p>
+        <p className="flex items-center gap-2.5 text-base">
+          <img src="./verified.svg" width={20} height={20} />
+          Stream
+        </p>
       </div>
-      <div className="border-slate-6 bg-slate-2 h-12 flex-1 border-b pr-2 pl-[21px]"></div>
+      <div className="border-slate-6 bg-slate-2 flex h-12 gap-2 flex-1 items-center border-b pr-2 pl-6.5">
+        <HashtagIcon className="size-5 text-slate-9" />
+        <p className="text-base">design-engineer-assignment</p>
+      </div>
     </div>
   );
 };
 
 export const Content = () => {
   return (
-    <section className="border-slate-4 flex-1 rounded-tl-xl border">
+    <section className="border-slate-4 flex flex-1 flex-col rounded-tl-xl border">
       <Navbar />
+      <div className="flex h-full">
+        <Channels />
+        <Messages />
+        <Members />
+      </div>
     </section>
   );
 };
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,23 @@
+import { HelpIcon } from "./icons/HelpIcon";
+import { InboxIcon } from "./icons/Inbox";
+
 export const Header = () => {
   return (
-    <header className="flex h-8 shrink-0 items-center justify-center gap-2">
-      <img
-        src="./stream.png"
-        width={20}
-        height={20}
-        className="size-5 rounded"
-      />
-      Stream
+    <header className="flex h-8 shrink-0 items-center justify-between px-4.5">
+      <div />
+      <div className="flex items-center gap-2">
+        <img
+          src="./stream.png"
+          width={20}
+          height={20}
+          className="size-5 rounded"
+        />
+        Stream
+      </div>
+      <div className="flex items-center gap-6">
+        <InboxIcon className="text-slate-9" />
+        <HelpIcon className="size-5 text-slate-9" />
+      </div>
     </header>
   );
 };
