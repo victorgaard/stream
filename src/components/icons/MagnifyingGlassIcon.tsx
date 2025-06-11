9a75d7aa61625a3265e370d6f55b66e0c9cf04c1
Victor F. Santos <victor.gaard@gmail.com>
2025-06-11 09:14:35 -0300

    feat: header icons

---
+++ src/components/icons/MagnifyingGlassIcon.tsx
@@ -0,0 +1,19 @@
+export const MagnifyingGlassIcon: React.FC<React.SVGProps<SVGSVGElement>> = ({
+  ...props
+}) => (
+  <svg
+    aria-hidden="true"
+    role="img"
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    fill="none"
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <path
+      fill="currentColor"
+      d="M17.3 18.7a1 1 0 0 0 1.4-1.4L13.42 12l5.3-5.3a1 1 0 0 0-1.42-1.4L12 10.58l-5.3-5.3a1 1 0 0 0-1.4 1.42L10.58 12l-5.3 5.3a1 1 0 1 0 1.42 1.4L12 13.42l5.3 5.3Z"
+    ></path>
+  </svg>
+);
